0bc494502867854c9e76de2467811f0fc1e339de
bruno <brunoparaaika@gmail.com>
2023-03-09 18:05:06 -0300

    organizado os templates em pastas.

--- project_ntdw/module_evento_premiacao/views.py
+++ project_ntdw/module_evento_premiacao/views.py
@@ -17,18 +17,20 @@ def index(request):
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
 
-
+# -------------------- CRONOGRAMAS --------------------------
 def cronogramas(request):
-    return render(request, 'cronogramas.html', {'cronogramas': Cronograma.objects.all()})
+    return render(request, 'cronograma/cronogramas.html', {'cronogramas': Cronograma.objects.all()})
+
 
 def cronograma_delete(request, id):
     cronograma = Cronograma.objects.get(id=id)
     cronograma.delete()
     return redirect(cronogramas)
 
+
 def cronograma_editar(request, id):
     if request.method == 'GET':
-        return render(request, 'cronograma_update.html', {'cronograma': Cronograma.objects.get(id=id)})
+        return render(request, 'cronograma/cronograma_update.html', {'cronograma': Cronograma.objects.get(id=id)})
     elif request.method == 'POST':
         cronograma = Cronograma.objects.get(id=id)
         cronograma.data_inicio = request.POST.get('data_inicial')
@@ -36,31 +38,36 @@ def cronograma_editar(request, id):
         cronograma.descricao = request.POST.get('descricao')
         cronograma.save()
         return redirect(cronogramas)
-def cronograma_create(request):
 
+
+def cronograma_create(request):
     if request.method == 'POST':
         dInicial = request.POST.get('data_inicial')
         dFinal = request.POST.get('data_final')
         vdescricao = request.POST.get('descricao')
-        Cronograma.objects.create(data_inicio = dInicial, data_final = dFinal, descricao=vdescricao)
+        Cronograma.objects.create(data_inicio=dInicial, data_final=dFinal, descricao=vdescricao)
         Cronograma.objects.all()
         return redirect(cronogramas)
     elif request.method == 'GET':
-        return render(request, 'cronograma_create.html', {})
+        return render(request, 'cronograma/cronograma_create.html', {})
 
-#-------------EVENTOS----------------#
+
+# -------------------- EVENTOS -----------------
+
 def eventos(request):
-    return render(request, 'eventos.html', {'eventos': Evento.objects.all()})
+    return render(request, 'evento/eventos.html', {'eventos': Evento.objects.all()})
+
 
 def evento_delete(request, id):
     evento = Evento.objects.get(id=id)
     evento.delete()
     return redirect(eventos)
 
+
 def evento_editar(request, id):
     if request.method == 'GET':
-        return render(request, 'evento_update.html',
-                      {'evento': Evento.objects.get(id=id), 'cronogramas':Cronograma.objects.all()})
+        return render(request, 'evento/evento_update.html',
+                      {'evento': Evento.objects.get(id=id), 'cronogramas': Cronograma.objects.all()})
     elif request.method == 'POST':
         evento = Evento.objects.get(id=id)
         evento.nome = request.POST.get('nome')
@@ -69,16 +76,17 @@ def evento_editar(request, id):
         evento.cronograma_fk = Cronograma.objects.get(id=request.POST.get('cronograma_fk'))
         evento.save()
         return redirect(eventos)
-def evento_create(request):
 
+
+def evento_create(request):
     if request.method == 'POST':
         nome = request.POST.get('nome')
         descricao = request.POST.get('descricao')
         ano = request.POST.get('ano')
         cronograma_fk = Cronograma.objects.get(id=request.POST.get('cronograma_fk'))
-        Evento.objects.create(nome=nome,descricao=descricao,ano=ano,cronograma_fk=cronograma_fk)
+        Evento.objects.create(nome=nome, descricao=descricao, ano=ano, cronograma_fk=cronograma_fk)
         Evento.objects.all()
 
         return redirect(eventos)
     elif request.method == 'GET':
-        return render(request, 'evento_create.html', {'cronogramas':Cronograma.objects.all()})
+        return render(request, 'evento/evento_create.html', {'cronogramas': Cronograma.objects.all()})
